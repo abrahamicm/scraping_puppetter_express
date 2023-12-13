8c803ae7aab3a2d6e202bf183bf2afa8f4e30484
Abraham Cordero <32270874+abrahamicm@users.noreply.github.com>
2023-12-13 00:29:53 -0400

    agreegando la opcion de eliminar elemento

--- src/scraping.js
+++ src/scraping.js
@@ -4,13 +4,14 @@ async function scrapeHandler(req, res) {
   const urls = req.body.urls.split('\n').map(url => url.trim());
   const strategy = req.body.strategy;
   const selector = req.body.selector;
+  const hideSelector = req.body.hideSelector;
 
-  const result = await scrapeUrls(urls, strategy, selector);
+  const result = await scrapeUrls(urls, strategy, selector, hideSelector);
 
   res.send(`Resultados: ${JSON.stringify(result, null, 2)}`);
 }
 
-async function scrapeUrls(urls, strategy, selector) {
+async function scrapeUrls(urls, strategy, selector, hideSelector = null) {
   const browser = await puppeteer.launch();
   const result = [];
 
@@ -18,11 +19,24 @@ async function scrapeUrls(urls, strategy, selector) {
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'networkidle2' });
 
-    if (strategy === 'selector' && selector) {
-      // Usa page.$$ para obtener todos los elementos que coinciden con el selector
-      const elementHandles = await page.$$(selector);
+    let elementHandles;
 
-      // Itera sobre cada elemento y captura la pantalla
+    if (strategy === 'selector' && selector) {
+      elementHandles = await page.$$(selector);
+
+      if (hideSelector) {
+        // Ejecuta código JavaScript en la página para eliminar elementos
+        await page.evaluate((hideSel) => {
+          const elements = document.querySelectorAll(hideSel);
+          for (const element of elements) {
+            element.parentNode.removeChild(element);
+          }
+        }, hideSelector);
+
+        // Agrega un tiempo de espera (ajusta según sea necesario)
+        await page.waitForTimeout(1000);
+      }
+
       for (let i = 0; i < elementHandles.length; i++) {
         const screenshotPath = `screenshot_${Date.now()}_${i}.png`;
         await elementHandles[i].screenshot({ path: screenshotPath });
@@ -42,4 +56,3 @@ module.exports = {
   scrapeHandler,
   scrapeUrls,
 };
-
